c9f4526ccede66b4a375eb32e17595c31162cd9d
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-11-21 09:25:31 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <router-view :foods = "foods"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data () {
+    return {
+      foods: [
+        {t: '青木瓜、玉米、蓮藕雜煮', i: '青木瓜、玉米、蓮藕雜煮.jpg'},
+        {t: '南瓜湯', i: '南瓜湯.jpg'},
+        {t: '餅', i: '餅.jpg'}
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <div class="ui animated cards">
+      <div class="ui card" v-for = "f in foods" v-bind:key="f">
+        <div class="image">
+          <img :src="'/static/images/' + f.i" />
+        </div>
+        <div class="ui header">
+          {{f.t}}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: ['foods'],
+  data () {
+    return {
+      msg: '自然美食DIY'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+.animated .card {
+  position: relative;
+  top: 0;
+}
+.animated .card:hover {
+  top: -2px;
+  transition: all 0.3s ease;
+}
+</style>
