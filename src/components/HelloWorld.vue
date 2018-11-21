60088ca26a35608529b64258e6b45fb83741e56b
Bestian Tang 唐宗浩 <besitan@gmail.com>
2018-11-21 10:18:46 +0800

    small

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <div class="ui animated cards">
-      <div class="ui card" v-for = "f in foods" v-bind:key="f">
+    <div class="ui from container">
+        <input v-model="mySearch" placeholder="以關鍵字搜詢" autofocus />
+    </div>
+    <hr/>
+    <div class="ui animated four doubling cards container">
+      <div class="ui card" v-for = "(f, index) in foods" v-bind:key="index" v-show = "has(f, mySearch)">
         <div class="image">
           <img :src="'/static/images/' + f.i" />
         </div>
         <div class="ui header">
           {{f.t}}
+        </div>
+        <div class="description" v-if="f.p">
+          作法：
+          <ol class="ui ordered list">
+            <li class="item" v-for = "(p, idx) in f.p" v-bind:key="idx">{{ p }}</li>
+          </ol>
         </div>
       </div>
     </div>
@@ -20,7 +30,14 @@ export default {
   props: ['foods'],
   data () {
     return {
-      msg: '自然美食DIY'
+      msg: '自然美食DIY',
+      mySearch: ''
+    }
+  },
+  methods: {
+    has: function (j, k) {
+      if (!k) return true
+      return JSON.stringify(j).indexOf(k) > -1
     }
   }
 }
