b3e74453956513311c0798160667cce65c575f21
Bestian Tang <bestian@gmail.com>
2019-11-05 06:57:48 +0800

    remove new ga, use old ga

--- src/main.js
+++ src/main.js
@@ -5,25 +5,10 @@ import App from './App'
 import router from './router'
 import autofocus from 'vue-autofocus-directive'
 import infiniteScroll from 'vue-infinite-scroll'
-import VueAnalytics from 'vue-analytics'
 
 Vue.directive('autofocus', autofocus)
 
 Vue.use(infiniteScroll)
-
-const isProd = process.env.NODE_ENV === 'production'
-
-Vue.use(VueAnalytics, {
-  id: 'UA-26178243-13',
-  router,
-  debug: {
-    enabled: !isProd,
-    sendHitTask: isProd
-  },
-  autoTracking: {
-    pageviewOnLoad: false
-  }
-})
 
 Vue.config.productionTip = false
 
