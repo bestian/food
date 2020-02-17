a6a0bac35f102db43f0da84e385ae2c55e0c20a9
Bestian Tang <bestian@gmail.com>
2020-02-18 07:56:24 +0800

    use image-gallary

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -11,20 +11,36 @@
       | 飲食區打食材關鍵字（如：紅豆）可以找到相關的料理，
       br
       | 祝大家食在喜悅、自然~
-    router-link(to="/item/93")
+    //router-link(to="/item/93")
       transition-group(name="list", tag="p")
         .ui(v-for="(f, idx) in foods", v-bind:key = "f.t", v-if="myIndex % foods.length == idx")
           img(:src = "'/static/images/' + f.i")
           h3 {{f.t}}
+    img(class="image", v-for="(image, i) in images", :src="image", :key="i", @click="index = i")
+    vue-gallery-slideshow(:images="images", :index="index", @close="index = null")
 </template>
 
 <script>
+
+import VueGallerySlideshow from 'vue-gallery-slideshow'
+
 export default {
   name: 'Intro',
   props: ['foods'],
+  components: {
+    VueGallerySlideshow
+  },
   data () {
     return {
+      index: undefined,
       myIndex: 93
+    }
+  },
+  computed: {
+    images: function () {
+      return this.foods.map(function (o) {
+        return '/static/images/' + o.i
+      })
     }
   },
   methods: {
@@ -70,6 +86,12 @@ img {
 .list-enter, .list-leave-to /* .list-leave-active below version 2.1.8 */ {
   opacity: 0;
   transform: scale(0.5);
+}
+
+.image {
+  cursor: pointer;
+  width: 10%;
+  height: auto;
 }
 
 </style>
