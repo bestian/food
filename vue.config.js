a9acef4fec63d66192929e9e2676d90bdd2371f3
Bestian Tang <bestian@gmail.com>
2020-12-14 15:06:31 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   pwa: {
     workboxOptions: {
-      exclude: ['*.gitkeep', '*.map'],
+      exclude: ['.gitkeep','*.gitkeep', '*.map'],
     }
   },
   publicPath: process.env.NODE_ENV === 'production'
