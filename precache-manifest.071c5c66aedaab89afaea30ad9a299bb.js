c4142c61c8b2284b2d10cbf61029d21182ac12cb
Bestian Tang 小巴 <bestian@gmail.com>
2021-07-07 18:45:17 +0800

    deploy

---
+++ precache-manifest.071c5c66aedaab89afaea30ad9a299bb.js
@@ -0,0 +1,398 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb1889183f8ce395cf3a",
+    "url": "/css/app.e6337edb.css"
+  },
+  {
+    "revision": "bb1889183f8ce395cf3a",
+    "url": "/css/app.e6337edb.css.map"
+  },
+  {
+    "revision": "2a80b72d6ff342a79aba",
+    "url": "/css/chunk-1f228739.a33b7b60.css"
+  },
+  {
+    "revision": "2a80b72d6ff342a79aba",
+    "url": "/css/chunk-1f228739.a33b7b60.css.map"
+  },
+  {
+    "revision": "790a749484aef4fa719f",
+    "url": "/css/chunk-2d6efae8.bd0ced16.css"
+  },
+  {
+    "revision": "790a749484aef4fa719f",
+    "url": "/css/chunk-2d6efae8.bd0ced16.css.map"
+  },
+  {
+    "revision": "3fc87b0563a06502470d",
+    "url": "/css/chunk-7f22ba0a.d413c8ab.css"
+  },
+  {
+    "revision": "3fc87b0563a06502470d",
+    "url": "/css/chunk-7f22ba0a.d413c8ab.css.map"
+  },
+  {
+    "revision": "ce1cfe88fdd835d5d655",
+    "url": "/css/chunk-vendors.e9d76111.css"
+  },
+  {
+    "revision": "ce1cfe88fdd835d5d655",
+    "url": "/css/chunk-vendors.e9d76111.css.map"
+  },
+  {
+    "revision": "34f91316d346e33c8e4bd9071681bb57",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/fonts/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/fonts/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/fonts/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/fonts/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/fonts/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/fonts/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/fonts/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/fonts/icons.faff9214.woff"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/fonts/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/fonts/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/fonts/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/fonts/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/img/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "70f93631cf7ce6655d0517211b09b8dd",
+    "url": "/img/elf.png"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/img/flags.9c74e172.png"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/img/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "d80de7c95ad36412419b9f83981e2969",
+    "url": "/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "8c867368bd1c7625d3d1e4b9a1f1d2c6",
+    "url": "/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "d80de7c95ad36412419b9f83981e2969",
+    "url": "/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "8c867368bd1c7625d3d1e4b9a1f1d2c6",
+    "url": "/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "794362690bc1544004175fb1358b9e9d",
+    "url": "/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "f7a9e2c621b7d566b86b49da2e444780",
+    "url": "/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "81e86739fa197bad916d3b94f7f443d1",
+    "url": "/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "acdf04259db90090cc888af7fe028df7",
+    "url": "/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "c77f60340a91c5eb534739df3e576ffc",
+    "url": "/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "81e86739fa197bad916d3b94f7f443d1",
+    "url": "/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "d04e79b3352505b4646e2251cad80fbf",
+    "url": "/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "535b90096b6b0110b2b16e0e334d5c7d",
+    "url": "/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "34f91316d346e33c8e4bd9071681bb57",
+    "url": "/img/icons/favicon.ico"
+  },
+  {
+    "revision": "ca9b72d123a80d2915c1292b975dfec1",
+    "url": "/img/icons/manifest.json"
+  },
+  {
+    "revision": "6e4c54bbd2585212f85b255b427a2e5c",
+    "url": "/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "23be00b05518c1e620518c3c3de2fc2e",
+    "url": "/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "28c3ec8ddd9e612df4afda0203719e05",
+    "url": "/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/img/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "84896acaf5133160e12052d08e51172c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb1889183f8ce395cf3a",
+    "url": "/js/app.2e4a5b84.js"
+  },
+  {
+    "revision": "bb1889183f8ce395cf3a",
+    "url": "/js/app.2e4a5b84.js.map"
+  },
+  {
+    "revision": "2a80b72d6ff342a79aba",
+    "url": "/js/chunk-1f228739.188e330d.js"
+  },
+  {
+    "revision": "2a80b72d6ff342a79aba",
+    "url": "/js/chunk-1f228739.188e330d.js.map"
+  },
+  {
+    "revision": "790a749484aef4fa719f",
+    "url": "/js/chunk-2d6efae8.e6b693e5.js"
+  },
+  {
+    "revision": "790a749484aef4fa719f",
+    "url": "/js/chunk-2d6efae8.e6b693e5.js.map"
+  },
+  {
+    "revision": "3fc87b0563a06502470d",
+    "url": "/js/chunk-7f22ba0a.41c87028.js"
+  },
+  {
+    "revision": "3fc87b0563a06502470d",
+    "url": "/js/chunk-7f22ba0a.41c87028.js.map"
+  },
+  {
+    "revision": "ce1cfe88fdd835d5d655",
+    "url": "/js/chunk-vendors.2faaab0c.js"
+  },
+  {
+    "revision": "ce1cfe88fdd835d5d655",
+    "url": "/js/chunk-vendors.2faaab0c.js.map"
+  },
+  {
+    "revision": "4c125114787dea01a104c33fcf8a6e8a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4433786ec4f7c035bc554932b860af54",
+    "url": "/og-image.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "71c5e5ca411851afd7b7781ae482e55c",
+    "url": "/static/foods.json"
+  },
+  {
+    "revision": "5c51e73527a4ceb2acab4b59bd54547d",
+    "url": "/static/images/月桃葉飯.jpeg"
+  },
+  {
+    "revision": "181278541a1381f3d06670e9df77550a",
+    "url": "/static/images/火龍珠珠球.jpeg"
+  },
+  {
+    "revision": "cc47cc5958457a8bc0d759bbf79a7672",
+    "url": "/static/images/煨絲瓜.jpeg"
+  },
+  {
+    "revision": "705497a2201309c7ab09c4b19b154dbf",
+    "url": "/static/images/爽口涮.jpeg"
+  },
+  {
+    "revision": "ae4ba17ef1acd4a3eb48b30cd98d62dd",
+    "url": "/static/images/竹筒飯.jpeg"
+  },
+  {
+    "revision": "e6a59afc0e3e299602ec78ff32c4a9fc",
+    "url": "/static/images/糙米餅.jpeg"
+  },
+  {
+    "revision": "c1058627a98822c8363823b012288f53",
+    "url": "/static/images/蘿蔔滷味.jpeg"
+  },
+  {
+    "revision": "38dd4fa1f12be659a290c6405c5abbb5",
+    "url": "/static/images/酸菜油芒炒飯.jpeg"
+  },
+  {
+    "revision": "7d93984abdc97c974546e1d8c8ff4daf",
+    "url": "/static/images/銀耳羹.jpeg"
+  },
+  {
+    "revision": "5a20b97472717d90410bb2e13d9d381c",
+    "url": "/static/images/鳳梨腰果醬.jpeg"
+  },
+  {
+    "revision": "c2f92bcb1edef29ff25088287d6c0d01",
+    "url": "/static/images/麻辣雜煮.jpeg"
+  },
+  {
+    "revision": "d80de7c95ad36412419b9f83981e2969",
+    "url": "/static/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "8c867368bd1c7625d3d1e4b9a1f1d2c6",
+    "url": "/static/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "d80de7c95ad36412419b9f83981e2969",
+    "url": "/static/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "8c867368bd1c7625d3d1e4b9a1f1d2c6",
+    "url": "/static/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "794362690bc1544004175fb1358b9e9d",
+    "url": "/static/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "f7a9e2c621b7d566b86b49da2e444780",
+    "url": "/static/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "81e86739fa197bad916d3b94f7f443d1",
+    "url": "/static/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "acdf04259db90090cc888af7fe028df7",
+    "url": "/static/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "c77f60340a91c5eb534739df3e576ffc",
+    "url": "/static/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "81e86739fa197bad916d3b94f7f443d1",
+    "url": "/static/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "380ab7cd143561142e493527db542f09",
+    "url": "/static/img/icons/browserconfig.xml"
+  },
+  {
+    "revision": "d04e79b3352505b4646e2251cad80fbf",
+    "url": "/static/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "535b90096b6b0110b2b16e0e334d5c7d",
+    "url": "/static/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "34f91316d346e33c8e4bd9071681bb57",
+    "url": "/static/img/icons/favicon.ico"
+  },
+  {
+    "revision": "53af4cc6c24002e68c31c002065198c7",
+    "url": "/static/img/icons/manifest.json"
+  },
+  {
+    "revision": "6e4c54bbd2585212f85b255b427a2e5c",
+    "url": "/static/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "23be00b05518c1e620518c3c3de2fc2e",
+    "url": "/static/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "28c3ec8ddd9e612df4afda0203719e05",
+    "url": "/static/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "5e4b69388447ca67c3dbd7d55f961df0",
+    "url": "/static/img/icons/site.webmanifest"
+  },
+  {
+    "revision": "3269cea75e3a077031972eacc8d0a02b",
+    "url": "/static/img/seafood.png"
+  },
+  {
+    "revision": "74d4dad68bdb64c7007f5b2ede18b494",
+    "url": "/static/img/veg.png"
+  },
+  {
+    "revision": "af035009629c81cbe5b836d6e881993f",
+    "url": "/static/img/water.svg"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/static/robots.txt"
+  },
+  {
+    "revision": "5b63ca13a166b143d6e6efdbc64303db",
+    "url": "/static/snacks.json"
+  },
+  {
+    "revision": "73ecbef970ad85da9352b79d1ca97922",
+    "url": "/static/tembei.json"
+  },
+  {
+    "revision": "8b5ff373cceefae6b7f03161e8c9cd8f",
+    "url": "/static/videos/紅豆腰果奶醬.mp4"
+  },
+  {
+    "revision": "2cf009faf36fee0029d77d30679c318a",
+    "url": "/static/videos/蜂蜜肉桂核桃.mp4"
+  },
+  {
+    "revision": "fcb7aa1446aecde9c11be20d478f5bc4",
+    "url": "/static/videos/蜂蜜肉桂核桃0.mp4"
+  }
+]);
